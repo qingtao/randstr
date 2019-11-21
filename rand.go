33a77b47fbf9e4b3650b4620960b5788297c4dd0
wuqingtao <wuqingtao@antlinker.com>
2019-11-21 21:44:28 +0800

    在初始化是设置随机种子

--- rand.go
+++ rand.go
@@ -1,4 +1,4 @@
-// Package randstr 随即字符串
+// Package randstr 随机字符串生成器
 package randstr
 
 import (
@@ -9,43 +9,39 @@ import (
 )
 
 var (
-	// 随即字符串来源
+	defaultSource *source
+	// 随机字符串来源
 	encodeStr = []byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
 )
 
-// New 生成一个新的长度为n的随即字符串
+// New 生成一个长度为n的随机字符串
 func New(n int) string {
 	return defaultSource.rand(n)
 }
 
-var defaultSource *source
-
-// Source 随即字符串生成器
 type source struct {
-	r *rand.Rand
-	p sync.Pool
+	sync.Pool
 }
 
-// RandString 随即字符串
+// rand 随机字符串
 func (s *source) rand(n int) string {
-	buf := s.p.Get().(*strings.Builder)
+	buf := s.Get().(*strings.Builder)
 	buf.Reset()
 	for i := 0; i < n; i++ {
 		buf.WriteByte(encodeStr[rand.Intn(len(encodeStr))])
 	}
 	str := buf.String()
-	buf.Reset()
-	s.p.Put(buf)
+	s.Put(buf)
 	return str
 }
 
 func init() {
 	defaultSource = &source{
-		p: sync.Pool{
+		Pool: sync.Pool{
 			New: func() interface{} {
 				return new(strings.Builder)
 			},
 		},
-		r: rand.New(rand.NewSource(time.Now().UnixNano())),
 	}
+	rand.Seed(time.Now().UnixNano())
 }
--- rand_test.go
+++ rand_test.go
@@ -8,17 +8,24 @@ import (
 )
 
 func ExampleNew() {
+	// 生成长度为6的随机字符串
 	s := randstr.New(6)
+	// output
 	fmt.Println(s)
 }
 
 func TestNew(t *testing.T) {
+	t.Parallel()
 	t.Run("4", func(t *testing.T) {
 		s := randstr.New(4)
 		t.Logf("%s", s)
 	})
 	t.Run("6", func(t *testing.T) {
 		s := randstr.New(6)
+		t.Logf("%s", s)
+	})
+	t.Run("8", func(t *testing.T) {
+		s := randstr.New(8)
 		t.Logf("%s", s)
 	})
 }
